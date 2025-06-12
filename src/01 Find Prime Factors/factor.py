64c0d8959afd4a98c638414bca9d65e9eef048ee
Lalith Gargeya <95236320+LGargeya@users.noreply.github.com>
2025-06-12 01:47:48 +0000

    returning prime factors as set to have only distinct values

--- src/01 Find Prime Factors/factor.py
+++ src/01 Find Prime Factors/factor.py
@@ -9,7 +9,6 @@ def get_prime_factors(number):
             divisor += 1
     return factors
 
-
 # commands used in solution video for reference
 if __name__ == '__main__':
     print(get_prime_factors(630))  # [2, 3, 3, 5, 7]
