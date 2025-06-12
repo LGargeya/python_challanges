94e21460f6679abb4be30a47bfc6918a6f775342
Lalith Gargeya <95236320+LGargeya@users.noreply.github.com>
2025-06-12 02:04:07 +0000

    another def to the sorting method. Here it joins the original word with the lower case word and then sorts it. After that we can ignore the lower case word and get the original word using slice function.

--- src/03 Sort a String/sort_words.py
+++ src/03 Sort a String/sort_words.py
@@ -1,7 +1,15 @@
+'''
 def sort_words(words):
-    return ' '.join(sorted(words.split(), key=str.casefold))
-
+    return ' '.join(sorted(words.split(), key=str.casefold)) # key = str.casefold tells to ignore the case while sorting
+'''
+def sort_words(input):
+    words = input.split()
+    words = [w.lower()+w for w in words]
+    words.sort()
+    words = [w[len(w)//2:] for w in words]
+    return ' '.join(words)
 
 # commands used in solution video for reference
 if __name__ == '__main__':
     print(sort_words('banana ORANGE apple'))  # apple banana ORANGE
+
