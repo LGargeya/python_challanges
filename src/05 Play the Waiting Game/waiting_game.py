3c29a4a1240c70d26ff80af39bff17f42d4080b4
Lalith Gargeya <95236320+LGargeya@users.noreply.github.com>
2025-06-12 02:23:12 +0000

    I have created and infinite loop so that there is no need to run the program again. Asked input also to break the game

--- src/05 Play the Waiting Game/waiting_game.py
+++ src/05 Play the Waiting Game/waiting_game.py
@@ -2,10 +2,11 @@ import time
 import random
 
 def waiting_game():
+
     target = random.randint(2, 4)  # target seconds to wait
     print(f'\nYour target time is {target} seconds')
 
-    input(' ---Press Enter to Begin--- ')
+    input(' ---Press Enter to Begin--- ' + 'or ---Press Ctrl+Z to end the game---')
     start = time.perf_counter()
 
     input(f'\n...Press Enter again after {target} seconds...')
@@ -18,8 +19,12 @@ def waiting_game():
         print(f'({target - elapsed :.3f} seconds too fast)')
     else:
         print(f'({elapsed - target :.3f} seconds too slow)')
-
+           
+    
 
 # commands used in solution video for reference
 if __name__ == '__main__':
-    waiting_game()
+    while True:
+        waiting_game()
+        
+
